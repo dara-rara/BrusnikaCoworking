44fb0f8917d0225426a3ec9a151fcb02ca4b0a66
dara-rara <glebova555dasha@gmail.com>
2024-10-18 19:39:20 +0500

    Initial commit

---
+++ src/main/java/com/example/BrusnikaCoworking/BrusnikaCoworkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.BrusnikaCoworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrusnikaCoworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrusnikaCoworkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BrusnikaCoworking/BrusnikaCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BrusnikaCoworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrusnikaCoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
