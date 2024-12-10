6cb6ac0238ba57e8ac2958ec7f9df80010576183
dara-rara <glebova555dasha@gmail.com>
2024-12-11 01:28:37 +0500

    docker 2.0

--- src/main/java/com/example/BrusnikaCoworking/adapter/web/auth/dto/JwtAuthenticationResponse.java
+++ src/main/java/com/example/BrusnikaCoworking/adapter/web/auth/dto/JwtAuthenticationResponse.java
@@ -2,5 +2,5 @@ package com.example.BrusnikaCoworking.adapter.web.auth.dto;
 
 import com.example.BrusnikaCoworking.domain.user.Role;
 
-public record JwtAuthenticationResponse(String tokenAccess, String tokeRefresh, Role role) {
+public record JwtAuthenticationResponse(String tokenAccess, String tokenRefresh, Role role) {
 }
