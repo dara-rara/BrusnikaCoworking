92d98a80b878cf77ce8a902f81758708e2b6c9b9
dara-rara <glebova555dasha@gmail.com>
2025-01-10 02:05:51 +0500

    edit7

--- src/main/java/com/example/BrusnikaCoworking/BrusnikaCoworkingApplication.java
+++ src/main/java/com/example/BrusnikaCoworking/BrusnikaCoworkingApplication.java
@@ -2,12 +2,14 @@ package com.example.BrusnikaCoworking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.BrusnikaCoworking.adapter.repository")
 public class BrusnikaCoworkingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BrusnikaCoworkingApplication.class, args);
