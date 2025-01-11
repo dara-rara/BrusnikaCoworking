d1df2f230529a1356186782e32f39b2ffc56db71
dara-rara <glebova555dasha@gmail.com>
2025-01-11 19:11:23 +0500

    edit settings

--- src/main/java/com/example/BrusnikaCoworking/BrusnikaCoworkingApplication.java
+++ src/main/java/com/example/BrusnikaCoworking/BrusnikaCoworkingApplication.java
@@ -6,12 +6,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.BrusnikaCoworking.adapter.repository")
 public class BrusnikaCoworkingApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"));
 		SpringApplication.run(BrusnikaCoworkingApplication.class, args);
 	}
 }
